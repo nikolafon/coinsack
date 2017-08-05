155bcec9d882b53eeab5ff2ff259e6ffbc53f45a
Nikola Nikolic <n.nikolic@eyefreight.com>
2017-08-05 14:41:11 +0200

    Removed some test code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
-import { ECPair } from 'bitcoinjs-lib';
 import { HttpModule, Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
 
 @NgModule({
   declarations: [
@@ -22,9 +20,6 @@ import 'rxjs/add/operator/toPromise';
 export class AppModule {
 
   constructor(private http: Http) {
-    // console.log(ECPair.makeRandom().getAddress());
-    console.log(http.get('blockchain.info/rawaddr/1CDrvMW7R5S1LCsGxEYfuUsdSznsSxH8tF?format=json')
-      .toPromise().then(response => response.json()));
   }
 
 }
