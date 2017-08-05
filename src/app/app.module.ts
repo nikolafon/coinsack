9d850a8c0e21bf93233201764890d85692e80fb5
Nikola Nikolic <n.nikolic@eyefreight.com>
2017-08-05 12:32:00 +0200

    Testing firebase redirect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import 'hammerjs';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule} from '@angular/material';
+import { MaterialModule } from '@angular/material';
+import { ECPair } from 'bitcoinjs-lib';
+import { HttpModule, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MaterialModule
+    BrowserModule, BrowserAnimationsModule, MaterialModule, HttpModule
   ],
   entryComponents: [],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private http: Http) {
+    // console.log(ECPair.makeRandom().getAddress());
+    console.log(http.get('blockchain.info/rawaddr/1CDrvMW7R5S1LCsGxEYfuUsdSznsSxH8tF?format=json')
+      .toPromise().then(response => response.json()));
+  }
+
+}
