6ebc4ca819d8508fd99bf6c009412efbc402a4a6
Nikola Nikolic <n.nikolic@eyefreight.com>
2017-08-20 09:30:49 +0200

    Logo and favicons implemented for different platforms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import 'hammerjs';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule} from '@angular/material';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
